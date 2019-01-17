ee804cab739501f335fd23d1e37db152944c43d7
Luca <luca@implast.it>
2019-01-17 11:15:33 +0100

    Commit

--- backend/modules/module_magazzino_update/index.js
+++ backend/modules/module_magazzino_update/index.js
@@ -53,15 +53,12 @@ module.exports.format = [{
 
 
 module.exports.callback = function (proxy) {
-    console.log(JSON.stringify(proxy.recv, true))
-    console.log(JSON.stringify(proxy.recv.popup, true))
-    console.log(JSON.stringify(proxy.recv.gruppo, true))
-
     if (proxy.recv.modified)
         proxy.recv.modified.forEach(e => {
-            proxy.getConnection().query(`INSERT INTO magazzino(id, descrizione, info, giacenza, prezzoEur, prezzoCents, gruppo, details) VALUES ('${proxy.secure(e.id)}', '${proxy.secure(e.desc)}', '${proxy.secure(e.info)}', '${proxy.secure(e.giacenza)}', '${parseInt(proxy.secure(("" + e.costo).split('.')[0]))}', '${parseInt(proxy.secure(("" + e.costo).split('.')[1]))}', '${proxy.secure(e.gruppo.id)}', '${JSON.stringify(proxy.secure(e.popup.id))}') ON DUPLICATE KEY UPDATE id='${proxy.secure(e.id)}', descrizione='${proxy.secure(e.desc)}', info='${proxy.secure(e.info)}', giacenza='${proxy.secure(e.giacenza)}', prezzoEur='${parseInt(proxy.secure(("" + e.costo).split('.')[0]))}', prezzoCents='${parseInt(proxy.secure(("" + e.costo).split('.')[1]))}', gruppo='${proxy.secure(e.gruppo.id)}', details='${JSON.stringify(proxy.secure(e.popup.id))}'`, (e, r) => {
-                console.log(e)
-            });
+            proxy.getConnection().query(`INSERT INTO magazzino(id, descrizione, info, giacenza, prezzoEur, prezzoCents, gruppo, details) VALUES ('${proxy.secure(e.id)}', '${proxy.secure(e.desc)}', '${proxy.secure(e.info)}', '${proxy.secure(e.giacenza)}', '${parseInt(proxy.secure(("" + e.costo).split('.')[0]))}', '${isNaN(parseInt(proxy.secure(("" + e.costo).split('.')[1]))) ? '0' : parseInt(proxy.secure(("" + e.costo).split('.')[1]))}', '${proxy.secure(e.gruppo.value)}', '${proxy.secure(e.popup.value)}') ON DUPLICATE KEY UPDATE id='${proxy.secure(e.id)}', descrizione='${proxy.secure(e.desc)}', info='${proxy.secure(e.info)}', giacenza='${proxy.secure(e.giacenza)}', prezzoEur='${parseInt(proxy.secure(("" + e.costo).split('.')[0]))}', prezzoCents='${isNaN(parseInt(proxy.secure(("" + e.costo).split('.')[1]))) ? '0' : parseInt(proxy.secure(("" + e.costo).split('.')[1]))}', gruppo='${proxy.secure(e.gruppo.value)}', details='${proxy.secure(e.popup.value)}'`, (err1, res1) => {
+                console.log(err1);
+            })
         });
-    proxy.send({state: true})
-}
+
+    proxy.send({state: true});
+};
